0b0d0d2ec7f0b0eee4088c6248745b1f32b69399
franck <carnet.franck.paul@gmail.com>
2018-03-09 16:08:31 +0100

    Various code formatting

--- _admin.php
+++ _admin.php
@@ -1,57 +1,59 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of bigfoot, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief bigfoot, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul and contributors
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if (!defined('DC_CONTEXT_ADMIN')) { return; }
+if (!defined('DC_CONTEXT_ADMIN')) {return;}
 
 // dead but useful code, in order to have translations
-__('bigfoot').__('Empowering footnotes');
+__('bigfoot') . __('Empowering footnotes');
 
-$core->addBehavior('adminBlogPreferencesForm',array('bigfootBehaviors','adminBlogPreferencesForm'));
-$core->addBehavior('adminBeforeBlogSettingsUpdate',array('bigfootBehaviors','adminBeforeBlogSettingsUpdate'));
+$core->addBehavior('adminBlogPreferencesForm', array('bigfootBehaviors', 'adminBlogPreferencesForm'));
+$core->addBehavior('adminBeforeBlogSettingsUpdate', array('bigfootBehaviors', 'adminBeforeBlogSettingsUpdate'));
 
 class bigfootBehaviors
 {
-	public static function adminBlogPreferencesForm($core,$settings)
-	{
-		# Style options
-		$styles = array(
-			__("Default") => 'default',
-			__("Bottom") => 'bottom',
-			__("Numeric") => 'numeric'
-		);
-		$settings->addNameSpace('bigfoot');
-		echo
-		'<div class="fieldset"><h4>Bigfoot</h4>'.
-		'<p><label class="classic">'.
-		form::checkbox('bigfoot_enabled','1',$settings->bigfoot->enabled).
-		__('Enable Bigfoot').'</label></p>'.
-		'<h5>'.__('Options').'</h5>'.
-		'<p><label for="bigfoot_style" class="classic">'.__('Style:').'</label>'.
-		form::combo('bigfoot_style',$styles,$settings->bigfoot->style).
-		'</p>'.
-		'<p><label for="bigfoot_hover" class="classic">'.
-		form::checkbox('bigfoot_hover','1',$settings->bigfoot->hover).
-		__('Activate on hover').'</label>'.'</p>'.
-		'<p><label for="bigfoot_single" class="classic">'.
-		form::checkbox('bigfoot_single','1',$settings->bigfoot->single).
-		__('Activate only in single entry context').'</label>'.'</p>'.
-		'</div>';
-	}
+    public static function adminBlogPreferencesForm($core, $settings)
+    {
+        # Style options
+        $styles = array(
+            __("Default") => 'default',
+            __("Bottom")  => 'bottom',
+            __("Numeric") => 'numeric'
+        );
+        $settings->addNameSpace('bigfoot');
+        echo
+        '<div class="fieldset"><h4>Bigfoot</h4>' .
+        '<p><label class="classic">' .
+        form::checkbox('bigfoot_enabled', '1', $settings->bigfoot->enabled) .
+        __('Enable Bigfoot') . '</label></p>' .
+        '<h5>' . __('Options') . '</h5>' .
+        '<p><label for="bigfoot_style" class="classic">' . __('Style:') . '</label>' .
+        form::combo('bigfoot_style', $styles, $settings->bigfoot->style) .
+        '</p>' .
+        '<p><label for="bigfoot_hover" class="classic">' .
+        form::checkbox('bigfoot_hover', '1', $settings->bigfoot->hover) .
+        __('Activate on hover') . '</label>' . '</p>' .
+        '<p><label for="bigfoot_single" class="classic">' .
+        form::checkbox('bigfoot_single', '1', $settings->bigfoot->single) .
+        __('Activate only in single entry context') . '</label>' . '</p>' .
+            '</div>';
+    }
 
-	public static function adminBeforeBlogSettingsUpdate($settings)
-	{
-		$settings->addNameSpace('bigfoot');
-		$settings->bigfoot->put('enabled',!empty($_POST['bigfoot_enabled']),'boolean');
-		$settings->bigfoot->put('style',$_POST['bigfoot_style']);
-		$settings->bigfoot->put('hover',!empty($_POST['bigfoot_hover']));
-		$settings->bigfoot->put('single',!empty($_POST['bigfoot_single']));
-	}
+    public static function adminBeforeBlogSettingsUpdate($settings)
+    {
+        $settings->addNameSpace('bigfoot');
+        $settings->bigfoot->put('enabled', !empty($_POST['bigfoot_enabled']), 'boolean');
+        $settings->bigfoot->put('style', $_POST['bigfoot_style']);
+        $settings->bigfoot->put('hover', !empty($_POST['bigfoot_hover']));
+        $settings->bigfoot->put('single', !empty($_POST['bigfoot_single']));
+    }
 }
--- _public.php
+++ _public.php
@@ -1,95 +1,94 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of bigfoot, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief bigfoot, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul and contributors
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+if (!defined('DC_RC_PATH')) {return;}
 
-$core->addBehavior('publicHeadContent',array('bigfootPublic','publicHeadContent'));
-$core->addBehavior('publicFooterContent',array('bigfootPublic','publicFooterContent'));
+$core->addBehavior('publicHeadContent', array('bigfootPublic', 'publicHeadContent'));
+$core->addBehavior('publicFooterContent', array('bigfootPublic', 'publicFooterContent'));
 
 class bigfootPublic
 {
-	public static function publicHeadContent($core)
-	{
-		$core->blog->settings->addNameSpace('bigfoot');
-		if (!$core->blog->settings->bigfoot->enabled) {
-			return;
-		}
+    public static function publicHeadContent($core)
+    {
+        $core->blog->settings->addNameSpace('bigfoot');
+        if (!$core->blog->settings->bigfoot->enabled) {
+            return;
+        }
 
-		if ($core->blog->settings->bigfoot->single) {
-			// Single mode only, check if post/page context
-			$urlTypes = array('post');
-			if ($core->plugins->moduleExists('pages')) {
-				$urlTypes[] = 'page';
-			}
-			if (!in_array($core->url->type,$urlTypes)) {
-				return;
-			}
-		}
+        if ($core->blog->settings->bigfoot->single) {
+            // Single mode only, check if post/page context
+            $urlTypes = array('post');
+            if ($core->plugins->moduleExists('pages')) {
+                $urlTypes[] = 'page';
+            }
+            if (!in_array($core->url->type, $urlTypes)) {
+                return;
+            }
+        }
 
-		$style = $core->blog->settings->bigfoot->style;
-		if (!in_array($style,array('default','bottom','numeric'))) {
-			$style = 'default';
-		}
+        $style = $core->blog->settings->bigfoot->style;
+        if (!in_array($style, array('default', 'bottom', 'numeric'))) {
+            $style = 'default';
+        }
 
-		if (version_compare(DC_VERSION,'2.9','<') && (DC_VERSION != '2.9-dev'))
-		{
-			// Use old way to load public resources (Dotclear < 2.9 only)
-			$url = $core->blog->getQmarkURL().'pf='.basename(dirname(__FILE__));
-			echo
-			'<link rel="stylesheet" type="text/css" href="'.$url.'/css/bigfoot-'.$style.'.css" />'."\n".
-			'<script type="text/javascript" src="'.$url.'/js/bigfoot.js"></script>'."\n";
-		} else {
-			echo
-			dcUtils::cssLoad($core->blog->getPF('bigfoot/css/bigfoot-'.$style.'.css')).
-			dcUtils::jsLoad($core->blog->getPF('bigfoot/js/bigfoot.js'));
-		}
-	}
+        if (version_compare(DC_VERSION, '2.9', '<') && (DC_VERSION != '2.9-dev')) {
+            // Use old way to load public resources (Dotclear < 2.9 only)
+            $url = $core->blog->getQmarkURL() . 'pf=' . basename(dirname(__FILE__));
+            echo
+                '<link rel="stylesheet" type="text/css" href="' . $url . '/css/bigfoot-' . $style . '.css" />' . "\n" .
+                '<script type="text/javascript" src="' . $url . '/js/bigfoot.js"></script>' . "\n";
+        } else {
+            echo
+            dcUtils::cssLoad($core->blog->getPF('bigfoot/css/bigfoot-' . $style . '.css')) .
+            dcUtils::jsLoad($core->blog->getPF('bigfoot/js/bigfoot.js'));
+        }
+    }
 
-	public static function publicFooterContent($core)
-	{
-		$core->blog->settings->addNameSpace('bigfoot');
-		if (!$core->blog->settings->bigfoot->enabled) {
-			return;
-		}
+    public static function publicFooterContent($core)
+    {
+        $core->blog->settings->addNameSpace('bigfoot');
+        if (!$core->blog->settings->bigfoot->enabled) {
+            return;
+        }
 
-		if ($core->blog->settings->bigfoot->single) {
-			// Single mode only, check if post/page context
-			$urlTypes = array('post');
-			if ($core->plugins->moduleExists('pages')) {
-				$urlTypes[] = 'page';
-			}
-			if (!in_array($core->url->type,$urlTypes)) {
-				return;
-			}
-		}
+        if ($core->blog->settings->bigfoot->single) {
+            // Single mode only, check if post/page context
+            $urlTypes = array('post');
+            if ($core->plugins->moduleExists('pages')) {
+                $urlTypes[] = 'page';
+            }
+            if (!in_array($core->url->type, $urlTypes)) {
+                return;
+            }
+        }
 
-		echo
-		'<script type="text/javascript">'."\n".
-		"//<![CDATA[\n".
-		'$(document).ready(function() {'."\n".
-			'var bigfoot = $.bigfoot({'."\n".
-				'anchorPattern: /(fn|footnote|note|wiki-footnote)[:\-_\d]/gi,'."\n".
-				'footnoteTagname: "p, li",'."\n".
-				($core->blog->settings->bigfoot->style == 'bottom' ?
-					'positionContent: false,'."\n" : '')."\n".
-				($core->blog->settings->bigfoot->hover ?
-					'activateOnHover: true,'."\n".
-					'deleteOnUnhover: true,'."\n".
-					'hoverDelay: 500,'."\n" :
-					'')."\n".
-				'numberResetSelector: ".post",'."\n".
-				'scope: ".post"'."\n".
-			'});'."\n".
-		"});\n".
-		"\n//]]>\n".
-		"</script>\n";
-	}
+        echo
+            '<script type="text/javascript">' . "\n" .
+            '$(document).ready(function() {' . "\n" .
+            'var bigfoot = $.bigfoot({' . "\n" .
+            'anchorPattern: /(fn|footnote|note|wiki-footnote)[:\-_\d]/gi,' . "\n" .
+            'footnoteTagname: "p, li",' . "\n" .
+            ($core->blog->settings->bigfoot->style == 'bottom' ?
+            'positionContent: false,' . "\n" : '') . "\n" .
+            ($core->blog->settings->bigfoot->hover ?
+            'activateOnHover: true,' . "\n" .
+            'deleteOnUnhover: true,' . "\n" .
+            'hoverDelay: 500,' . "\n" :
+            '') . "\n" .
+            'numberResetSelector: ".post",' . "\n" .
+            'scope: ".post"' . "\n" .
+            '});' . "\n" .
+            "});\n" .
+            "</script>\n";
+    }
 }
