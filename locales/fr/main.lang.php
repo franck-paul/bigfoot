485be91a9e70030fb8cb7ce68a0cc68b154f1908
franck <carnet.franck.paul@gmail.com>
2023-05-20 08:19:40 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -13,12 +13,12 @@ use Dotclear\Helper\L10n;
 
 L10n::$locales['Empowering footnotes'] = 'Autonomisatateur de note';
 L10n::$locales['bigfoot'] = 'bigfoot';
-L10n::$locales['Default'] = '';
+L10n::$locales['Default'] = 'Défaut';
 L10n::$locales['Bottom'] = 'Pied de page';
 L10n::$locales['Numeric'] = 'Numérique';
 L10n::$locales['Enable Bigfoot'] = 'Activer Bigfoot';
-L10n::$locales['Options'] = '';
+L10n::$locales['Options'] = 'Options';
 L10n::$locales['Style:'] = 'Style :';
 L10n::$locales['Activate on hover'] = 'Activer au survol';
 L10n::$locales['Activate only in single entry context'] = 'Activer uniquement dans les contextes d\'entrée unique (billet ou page)';
-L10n::$locales['Bigfoot'] = 'bigfoot';
+L10n::$locales['Bigfoot'] = 'Bigfoot';
