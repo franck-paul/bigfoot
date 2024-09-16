1ceab7a8d001b8a73a7c51379924c3a0f5bbb216
franck <carnet.franck.paul@gmail.com>
2024-09-16 16:03:06 +0200

    New release 3.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'bigfoot',
     'Empowering footnotes',
     'Franck Paul and contributors',
-    '3.1',
+    '3.2',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
