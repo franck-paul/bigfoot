79aab47caf0a7565dc3c0424fdf8e45829c4a7a8
franck <contact@open-time.net>
2026-08-13 16:36:29 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,23 +11,27 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0
  */
-$this->registerModule(
-    'bigfoot',
-    'Empowering footnotes',
-    'Franck Paul and contributors',
-    '5.0',
-    [
-        'date'        => '2026-08-03T09:45:50+0200',
-        'requires'    => [['core', '2.39']],
-        'permissions' => 'My',
-        'type'        => 'plugin',
-        'settings'    => [
-            'blog' => '#params.bigfoot',
-        ],
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=bigfoot',
-        'support'    => 'https://github.com/franck-paul/bigfoot',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/bigfoot/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'bigfoot',
+        'Empowering footnotes',
+        'Franck Paul and contributors',
+        '5.0',
+        [
+            'date'        => '2026-08-03T09:45:50+0200',
+            'requires'    => [['core', '2.39']],
+            'permissions' => 'My',
+            'type'        => 'plugin',
+            'settings'    => [
+                'blog' => '#params.bigfoot',
+            ],
+
+            'details'    => 'https://open-time.net/?q=bigfoot',
+            'support'    => 'https://github.com/franck-paul/bigfoot',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/bigfoot/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
